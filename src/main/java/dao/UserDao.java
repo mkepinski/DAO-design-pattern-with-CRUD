bbfe9b9f47596b23df8cbce2169e44b97c7d7889
mkepinski <kepinski.poznan@gmail.com>
2021-07-28 12:35:34 +0200

    Version 1.0, first push.

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -14,15 +14,18 @@ import main.DBUtil;
 
 public class UserDao {
 
+	
 	private static final String CREATE_USER_QUERY = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
 	private static final String READ_USER_QUERY = "SELECT * FROM users WHERE id = ?";
 	private static final String UPDATE_USER_QUERY = "UPDATE users SET email = ?, username = ?, password = ? WHERE id = ?";
 	private static final String DELETE_USER_QUERY = "DELETE FROM users WHERE id = ?";
 	private static final String FIND_ALL_USERS_QUERY = "SELECT * FROM users";
 	
+	
 	public String hashPassword(String password) {
 		    return BCrypt.hashpw(password, BCrypt.gensalt());
 		}
+	
 	
 	public User create(User user) {
 		try(Connection conn = DBUtil.getConnection()) {
