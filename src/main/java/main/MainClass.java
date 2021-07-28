a8cce0e59da71dbaae8d4fbbbca13b38ce030b6f
mkepinski <kepinski.poznan@gmail.com>
2021-07-28 12:32:11 +0200

    Version 1.0, first push

--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -1,8 +1,6 @@
 package main;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.math.BigDecimal;
 
+import java.math.BigDecimal;
 import dao.CarDao;
 import dao.TransactionDao;
 import dao.UserDao;
